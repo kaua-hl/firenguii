cd5a3d2f13cbc2432a0d049b82a1a21b5f84e9de
Kaua Hl <profissional.kaua@gmail.com>
2022-01-08 14:25:11 -0300

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <SmallCardDisplay 
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections,
+    };
+  },
+};
+</script>
---
+++ pages/my-items.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <MyItem 
+            v-for="item in this.$store.state.myRentals"
+            :key="item.id"
+            :item="item"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: "no-nav",
+    }
+</script>
